d07ce7a1b33f02cf3f9d5626c6da1c0be8e19f55
Michael <michael.melendezm@gmail.com>
2016-09-12 09:33:43 -0600

    se hizo algo

---
+++ Data/ConnetionDB.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class ConnetionDB {
+        
+    public $server;
+    public $user;
+    public $password;
+    public $nameDB;
+        
+    
+    public function ConnetionDB(){
+        $this->server = "";
+        $this->user = "";
+        $this->password = "";
+        $this->db = "";
+    }
+    
+}
