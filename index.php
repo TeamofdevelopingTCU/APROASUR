456356bf130f766f56730db0812200d0b7dd20b4
DaylanChavarria <daylanchavarriaucr@gmail.com>
2016-09-15 23:12:35 -0600

    Agregue el video

--- index.php
+++ index.php
@@ -62,7 +62,7 @@
                         <span class="icon-bar"></span>
                     </button>
                     <a class="navbar-brand" href="#">
-                        <h4 style="background-color: #ffffff">Daylan Chavarría UCR Michael TCU Test </h4>
+                        <h4 style="color: #fff; font-size: 25px">APROASUR</h4>
                     </a>
                 </div><!--Navbar header End-->
                 <nav class="collapse navbar-collapse navigation" id="bs-example-navbar-collapse-1" role="navigation">
